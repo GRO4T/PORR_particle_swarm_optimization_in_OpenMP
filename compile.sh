3e2b7b6581b4eccc6b32957491016ddbefba23d0
Damian Kolaska <damian.kolaska99@gmail.com>
2022-10-27 22:04:01 +0200

    Added plot programs building to CMake

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ $1 src/test_functions.cpp -std=c++11 -I/usr/include/python3.10 -lpython3.10
--- src/plot_test_func2.cpp
+++ src/plot_test_func2.cpp
@@ -11,9 +11,9 @@ namespace plt = matplotlibcpp;
 int main()
 {
     std::vector<std::vector<double>> x, y, z;
-    for (double i = -5; i <= 5;  i += 0.25) {
+    for (double i = -40; i <= 40;  i += 2) {
         std::vector<double> x_row, y_row, z_row;
-        for (double j = -5; j <= 5; j += 0.25) {
+        for (double j = -40; j <= 40; j += 2) {
             x_row.push_back(i);
             y_row.push_back(j);
             z_row.push_back(testFunc2({i,j}));
