83b4b5ed8c2b869d99facd4d666981c42407f3d8
Damian Kolaska <damian.kolaska99@gmail.com>
2022-10-30 11:02:21 +0100

    Improving runner

--- src/programs/main.cpp
+++ src/programs/main.cpp
@@ -7,11 +7,15 @@
 
 using namespace std;
 
+void print_as_json(const SearchResult& search_result);
+
 int main(int argc, char** argv)
 {
     int n = 20;
     int threads = 4;
     int iterations = 100000;
+    int objective_func_id = 1;
+    std::string search_algorithm = "random";
 
     for (int i = 0; i < argc; ++i) {
         if (strcmp(argv[i], "-n") == 0) {
@@ -20,33 +24,48 @@ int main(int argc, char** argv)
             threads = stoi(argv[i+1]);
         } else if (strcmp(argv[i], "--iterations") == 0) {
             iterations = stoi(argv[i+1]);
-        }
+        } else if (strcmp(argv[i], "--search") == 0) {
+            search_algorithm = stoi(argv[i+1]);
+        } else if (strcmp(argv[i], "--obj_func") == 0) {
+            objective_func_id = stoi(argv[i+1]);
+        } 
     }
 
+    std::cout << "---------------------------------------" << std::endl;
     std::cout << "n: " << n << std::endl;
     std::cout << "threads: " << threads << std::endl;
     std::cout << "iterations: " << iterations << std::endl;
+    std::cout << "search_algorithm: " << search_algorithm << std::endl;
+    std::cout << "objective_func_id: " << objective_func_id << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
 
-    RandomSearch random_search(testFunc1, n, threads);
-    SearchResult searchResult1 = random_search.search(iterations);
+    SearchResult search_result;
+    ObjectiveFunc objective_func = objective_func_id == 1 ? testFunc1 : testFunc2;
 
-    cout << "Wyszukiwanie losowe:\n";
-    cout << "Najlepszy wynik: " << searchResult1.result << "\n";
-    cout << "{";
-    for(auto x : searchResult1.x)
+    if (search_algorithm == "random") {
+        RandomSearch random_search(objective_func, n, threads);
+        search_result = random_search.search(iterations);
+    } else if (search_algorithm == "swarm") {
+        int particle_count = 100;
+        SwarmSearch swarmSearch(objective_func, n, particle_count, threads);
+        SearchResult searchResult2 = swarmSearch.search(iterations);
+    }
+
+    std::cout << "Search: " << search_algorithm << std::endl;
+    std::cout << "Result:\n";
+    print_as_json(search_result);
+}
+
+void print_as_json(const SearchResult& search_result) {
+    cout << "{\n";
+    std::cout << "\t\"value\": " << search_result.result << "\n";
+    std::cout << "\t\"position\": [";
+    for(auto x : search_result.x)
         cout << x << ", ";
-    cout << "}\n";
-    cout << searchResult1.time << "[ns]\n";
+    std::cout << "]\n";
 
-    int particle_count = 100;
-    SwarmSearch swarmSearch(testFunc1, n, particle_count, threads);
-    SearchResult searchResult2 = swarmSearch.search(iterations);
+    std::cout << "\t\"exec_time_in_nanos\": " << search_result.time << "\n";
+    std::cout << "\t\"exec_time\": " << search_result.time / 1000000000.0 << "\n";
 
-    cout << "Wyszukiwanie rojem cząstek:\n";
-    cout << "Najlepszy wynik: " << searchResult2.result << "\n";
-    cout << "{";
-    for(auto x : searchResult2.x)
-        cout << x << ", ";
-    cout << "}\n";
-    cout << searchResult2.time << "[ns]\n";
+    std::cout << "}\n";
 }
